2ccb2eeded2b166a5cefd9b6b3ee3e98b0d2e726
denyskoech <dennis96ist@gmail.com>
2016-02-22 09:59:34 +0300

    Using Python, have the function xoxo(str) take the str parameter being passed and return the string true if there is an equal number of x's and o'

---
+++ xoxo.py
@@ -0,0 +1,12 @@
+def xoxo(str):
+    count = str.count('x')
+    count1 = str.count('o')
+    
+    if count == count1:
+        return print("True")
+    
+    else:
+        return print("False")
+    
+        
+xoxo("xoxxxxooxxoxxx")
